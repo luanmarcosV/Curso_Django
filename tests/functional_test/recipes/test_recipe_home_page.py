2c3c561b64f7154bd4b750f073bdddf600477333
Luan M. Valério <luanvalerio96@gmail.com>
2023-02-05 21:53:59 -0300

    Ajuste do teste da home page com o Selenium

--- tests/functional_test/recipes/test_recipe_home_page.py
+++ tests/functional_test/recipes/test_recipe_home_page.py
@@ -3,20 +3,17 @@ from unittest.mock import patch
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 from .base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here', body.text)
-        
-    # ----------------------------------------------------------------
-    # Test do campo de pesquisa
+        self.assertIn('No recipes found here 🥲', body.text)
+
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
         recipes = self.make_recipe_in_batch()
@@ -45,10 +42,8 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
         )
-        
-    # ----------------------------------------------------------------
-    # Teste de paginação
-    @patch('recipes.views.PER_PAGE', new=2)
+
+    @patch('recipes.views.site.PER_PAGE', new=2)
     def test_recipe_home_page_pagination(self):
         self.make_recipe_in_batch()
 
