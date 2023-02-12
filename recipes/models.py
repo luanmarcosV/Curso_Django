cf6dce9eec2b0ef68e644bd2235888975512acd7
Luan M. Valério <luanvalerio96@gmail.com>
2023-02-12 15:15:43 -0300

    Alteração do models.py no recipes "Precisa verificar a importação do Tag"

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,4 @@
-import os
-import string
 from collections import defaultdict
-from random import SystemRandom
-
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import F, Value
@@ -11,47 +6,70 @@ from django.db.models.functions import Concat
 from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
-from django.utils.translation import gettext_lazy as _
-from PIL import Image
-
-# Create your models here.
-
-
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
-
     def __str__(self):
         return self.name
-
-
+    
+class RecipeManager(models.Manager):
+    def get_published(self):
+        return self.filter(
+            is_published=True
+        ).annotate(
+            author_full_name=Concat(
+                F('author__first_name'), Value(' '),
+                F('author__last_name'), Value(' ('),
+                F('author__username'), Value(')'),
+            )
+        ).order_by('-id')
+        
 class Recipe(models.Model):
+    objects = RecipeManager()
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.TextField()
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.TextField()
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True,
-        default=None,)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+    #tags = models.ManyToManyField(Tag, blank=True, default='') PRECISA RESOLVER ISSO
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('recipe', args=(self.id,))
+        return reverse('recipes:recipe', args=(self.id,))
     
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
-
         return super().save(*args, **kwargs)
+    
+    def clean(self, *args, **kwargs):
+        error_messages = defaultdict(list)
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_messages['title'].append(
+                    'Found recipes with the same title'
+                )
+        if error_messages:
+            raise ValidationError(error_messages)
--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,44 @@
-import os
 from django.db.models import Q
+from django.db.models.aggregates import Count
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
+from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from utils.pagination import make_pagination
 from recipes.models import Recipe
+import os
+
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+def theory(request, *args, **kwargs):
+    recipes = Recipe.objects.get_published()
+    number_of_recipes = recipes.aggregate(number=Count('id'))
+    context = {
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number']
+    }
+    return render(
+        request,
+        'recipes/pages/theory.html',
+        context=context
+    )
+
+
 class RecipeListViewBase(ListView):
+    
     model = Recipe
     context_object_name = 'recipes'
     ordering = ['-id']
     template_name = 'recipes/pages/home.html'
+    
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
             is_published=True,
         )
-        qs = qs.select_related('author', 'category')
+        qs = qs.select_related('author', 'category', 'author__profile')
+        qs = qs.prefetch_related('tags')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -31,10 +52,15 @@ class RecipeListViewBase(ListView):
             {'recipes': page_obj, 'pagination_range': pagination_range}
         )
         return ctx
+    
 class RecipeListViewHome(RecipeListViewBase):
+    
     template_name = 'recipes/pages/home.html'
+    
 class RecipeListViewHomeApi(RecipeListViewBase):
+    
     template_name = 'recipes/pages/home.html'
+    
     def render_to_response(self, context, **response_kwargs):
         recipes = self.get_context_data()['recipes']
         recipes_list = recipes.object_list.values()
@@ -42,14 +68,18 @@ class RecipeListViewHomeApi(RecipeListViewBase):
             list(recipes_list),
             safe=False
         )
+        
 class RecipeListViewCategory(RecipeListViewBase):
+    
     template_name = 'recipes/pages/category.html'
+    
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
         ctx.update({
             'title': f'{ctx.get("recipes")[0].category.name} - Category | '
         })
         return ctx
+    
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
@@ -58,8 +88,34 @@ class RecipeListViewCategory(RecipeListViewBase):
         if not qs:
             raise Http404()
         return qs
+    
+class RecipeListViewTag(RecipeListViewBase):
+    
+    template_name = 'recipes/pages/tag.html'
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')
+        ).first()
+        if not page_title:
+            page_title = 'No recipes found'
+        page_title = f'{page_title} - Tag |'
+        ctx.update({
+            'page_title': page_title,
+        })
+        
+        return ctx
+    
 class RecipeListViewSearch(RecipeListViewBase):
+    
     template_name = 'recipes/pages/search.html'
+    
     def get_queryset(self, *args, **kwargs):
         search_term = self.request.GET.get('q', '')
         if not search_term:
@@ -71,7 +127,9 @@ class RecipeListViewSearch(RecipeListViewBase):
                 Q(description__icontains=search_term),
             )
         )
+        
         return qs
+    
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
         search_term = self.request.GET.get('q', '')
@@ -80,18 +138,44 @@ class RecipeListViewSearch(RecipeListViewBase):
             'search_term': search_term,
             'additional_url_query': f'&q={search_term}',
         })
+        
         return ctx
+    
 class RecipeDetail(DetailView):
+    
     model = Recipe
     context_object_name = 'recipe'
     template_name = 'recipes/pages/recipe-view.html'
+    
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published=True)
         return qs
+    
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
         ctx.update({
             'is_detail_page': True
         })
         return ctx
+    
+class RecipeDetailAPI(RecipeDetail):
+    
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+        
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+        
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
